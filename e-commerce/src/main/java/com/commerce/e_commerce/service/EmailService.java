f6db31516c1506c7f14bcf00b141814dc4320307
VaibhavGagneja <vaibhavgagneja@gmail.com>
2025-12-24 20:30:25 +0530

    Deploy trial

--- e-commerce/src/main/java/com/commerce/e_commerce/service/EmailService.java
+++ e-commerce/src/main/java/com/commerce/e_commerce/service/EmailService.java
@@ -21,24 +21,25 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EmailService {
 
-    private final JavaMailSender mailSender;
-    private final JavaMailSender javaMailSender;
+//    private final JavaMailSender mailSender;
+//    private final JavaMailSender javaMailSender;
     @Value("${BREVO_API_KEY}")
     private String brevoApiKey;
-    public void sendVerificationOtpEmail(String userEmail, String otp, String subject, String text) {
 
-        try {
-            MimeMessage mimeMessage = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
-            helper.setSubject(subject);
-            helper.setText(text);
-            helper.setTo(userEmail);
-            javaMailSender.send(mimeMessage);
-        } catch (Exception e) {
-
-            throw new MailSendException("Failed to send email to " + userEmail, e);
-        }
-    }
+//    public void sendVerificationOtpEmail(String userEmail, String otp, String subject, String text) {
+//
+//        try {
+//            MimeMessage mimeMessage = mailSender.createMimeMessage();
+//            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+//            helper.setSubject(subject);
+//            helper.setText(text);
+//            helper.setTo(userEmail);
+//            javaMailSender.send(mimeMessage);
+//        } catch (Exception e) {
+//
+//            throw new MailSendException("Failed to send email to " + userEmail, e);
+//        }
+//    }
 
     public void sendVerificationOtpEmail(String userEmail, String otp) {
         // 1. Configure the Brevo Client
